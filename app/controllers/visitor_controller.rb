701134c693ef020b147cff2a2ade920c0f868d02
Divam <dfordivam@gmail.com>
2011-05-16 20:46:05 +0530

    Added views for visitor/new

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -1,4 +1,6 @@
 class VisitorController < ApplicationController
   def new
+    @visitor = Visitor.new
+    @coll = ["BK" , "Non BK"] #_visitor_types
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Orc::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
+  match 'visitor/new' => 'visitor#new', :as => :visitors
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
@@ -54,5 +55,5 @@ Orc::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+   match ':controller(/:action(/:id(.:format)))'
 end
