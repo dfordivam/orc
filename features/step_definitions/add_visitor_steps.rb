6aa8d5bd1f54c2dff49a0c8ec6f84838638e32d0
Divam <dfordivam@gmail.com>
2011-05-17 05:55:49 +0530

    enhanced the feature to fill_in form

--- features/step_definitions/add_visitor_steps.rb
+++ features/step_definitions/add_visitor_steps.rb
@@ -1,13 +1,13 @@
 Given /^a visitor named "([^"]*)"$/ do |arg1|
-  @visitor_attr = { name: "name_test" , age: 23}
+  @visitor_attr = { name: "name_test" , age: 23, gender: "Male", address: "example address" , mobile_no: "9898989898", visitor_type: "BK" }
 end
 
 When /^I click on add visitor$/ do
-  visit add_visitor
+  visit visitors_path
 end
 
 When /^I enter the visitor details$/ do
-  pending # express the regexp above with the code you wish you had
+  fill_in "Name", with: @visitor_attr[:name]
 end
 
 When /^I click the "([^"]*)" button$/ do |arg1|
