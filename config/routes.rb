3257fa48c84e2b8785fcb2a744978c54991899a7
ravikanth.bk <ravikanth.bk@gmail.com>
2011-06-20 10:25:35 +0530

    layout change

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ Orc::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "checkins#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
