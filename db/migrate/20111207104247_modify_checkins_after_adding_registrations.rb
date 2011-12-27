3f5eb1ed13d35d8d85241c1a0c9bc4bfc20d74b8
dfordivam <dfordivam@gmail.com>
2011-12-27 19:25:25 +0530

    Modified some migrations related to checkin

--- db/migrate/20111207104247_modify_checkins_after_adding_registrations.rb
+++ db/migrate/20111207104247_modify_checkins_after_adding_registrations.rb
@@ -1,7 +1,7 @@
 class ModifyCheckinsAfterAddingRegistrations < ActiveRecord::Migration
   def self.up
     change_table :checkins do |t|
-      t.remove :room_id, :is_accom_req, :meals_req, :is_sight_seeing_req
+      t.remove :is_accom_req, :meals_req, :is_sight_seeing_req
 
       t.integer :registration_id
       t.date :checkout_date
@@ -12,7 +12,7 @@ class ModifyCheckinsAfterAddingRegistrations < ActiveRecord::Migration
 
   def self.down
     change_table :checkins do |t|
-      t.integer :room_id, :meals_req
+      t.integer :meals_req
       t.boolean :is_accom_req, :is_sight_seeing_req
 
       t.remove :registration_id, :checkout_date, :checkout_time
