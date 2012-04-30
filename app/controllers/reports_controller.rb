35dd138c726d368906d64ec6f980489d53e88a0b
Divam <dfordivam@gmail.com>
2012-04-30 06:01:07 +0000

    some basic func

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,11 @@
 class ReportsController < ApplicationController
+  before_filter :login_required
+  def index
+    @search_value  = params[:search_value]
+    if @search_value
+      @visitors = Visitor.search @search_value, :conditions => { :is_delete => '0' }
+    else 
+      @visitors = Visitor.where(:is_delete => false).order(:name,:address).paginate(:page => params[:page], :per_page => 15)
+    end
+  end
 end
